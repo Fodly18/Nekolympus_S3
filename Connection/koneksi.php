a3a32c989fb14280004cec311784286b43f5dec9
Fodly18 <alonamasaya@gmail.com>
2024-10-28 08:55:33 +0700

    perubahan pada path asset pada login.php

--- Connection/koneksi.php
+++ Connection/koneksi.php
@@ -2,7 +2,7 @@
 $host       = "localhost";
 $username   = "root";
 $password   = "";
-$db_name    = "db_sdkalisat";
+$db_name    = "web_mah";
 
 $connection = new mysqli($host, $username,$password,$db_name);
     if(!$connection){
