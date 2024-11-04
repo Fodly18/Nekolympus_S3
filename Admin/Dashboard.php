a20b577f12849749a4901e1c3c34aa8a390e82e6
LaLuMii <ululh777@gmail.com>
2024-11-04 07:40:10 +0700

    fix login

--- Admin/Dashboard.php
+++ Admin/Dashboard.php
@@ -1,3 +1,13 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['valid']) || !isset($_SESSION['username'])) {
+    // Jika belum login, redirect ke halaman login
+    header("Location: ../login.php");
+    exit();
+}
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,6 +19,5 @@
 <?php 
 include '../layouts/adminside.php'; 
 ?>
-
 </body>
 </html>
