b76b578b7a15c8cc2b96fd07fbaddab28aa47106
LaLuMii <ululh777@gmail.com>
2024-11-25 06:49:32 +0700

    dikit dikit jadi bukit

---
+++ app/models/Tugas.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Nekolympus\Project\models;
+
+use Nekolympus\Project\core\Model;
+
+class Tugas extends Model
+{
+    protected static $table = 'tugas';
+    protected static $guarded = ['id'];
+}
